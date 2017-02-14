21f517a2bd25d6dde9b560f2439592ffcc9cf5f3
vkinsella <vkinsella@D1016-08>
2017-02-14 13:59:16 +0000

    Add project files.

---
+++ NunitDemo/Postage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitDemo
+{
+    public class Postage
+    {
+        static void Main()
+        {  }
+ 	public int GetRegionCost(int region)
+ 	{
+ 	        switch(region)
+                {
+ 			        case 1: return 10;
+ 			        case 2: return 15;
+ 			        case 3: return 20;
+ 			        case 4: return 25;
+ 			        default: return 0;
+ 	           }
+            }
+
+         public double GetPostageCost(int region, double parcelWeight)
+         {
+ 	        return parcelWeight* 0.05 + GetRegionCost(region);
+         }
+
+    }
+}
---
+++ NunitDemoTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NUnit.Framework;
+using NunitDemo;
+
+namespace Example
+{
+    [TestFixture]
+    public class PostageTest
+    {
+        static void Main()
+        {
+
+        }
+
+        const double tol = 0.001;
+        Postage p1;
+
+        [SetUp]
+        public void Init()
+        {
+            p1 = new Postage();
+        }
+
+
+        [Test]
+        public void Test1()
+        {
+
+            Assert.That(p1, Is.Not.Null, "Object not created");
+        }
+
+
+        [Test]
+        public void TestPostageCalculation1()
+        {
+
+            double expectedResult = 5.0;
+            double actualResult = p1.GetPostageCost(1, 1500);
+            Assert.That(actualResult, Is.EqualTo(expectedResult), "Test of postage Failed for region 1 and weight 1500", tol);
+        }
+
+        [TestCase(3, 1000, ExpectedResult = 70)]
+        [TestCase(4, 1000, ExpectedResult = 75)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+
+        public double test3(int a, double b)
+        {
+
+            double actualResult = p1.GetPostageCost(a, b);
+            return actualResult;
+
+        }
+
+
+
+    }
+
+}
